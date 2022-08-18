ee8eba639b6a873357b47fe3378c1cf6f9532fcc
niloofararazkhani <niloofarbusiness@Niloofars-MacBook-Pro.local>
2022-08-18 23:42:37 +0430

    fix(font)

--- src/frames/AboutMe.js
+++ src/frames/AboutMe.js
@@ -14,23 +14,14 @@ export default function AboutMe() {
     <div className={style.aboutMe}>
       <div className={style.aboutMe__container}>
         <div className={style.aboutMe__body}>
-        <img src={image} className={style.aboutMe__profile} />
-        <div className={style.aboutMe__description}>
-
-      <h1 className={style.aboutMe__title}>About Me</h1>
-          <p >
-            The saying goes, if you love what you do, you can make it your job,
-            and creating is my hobby. If I think back to my whole life, the
-            times when I was creating a work, I was fine. If you look at my
-            resume you may wonder why I went into the academic field when I am
-            interested in creating, and I answer that studying and science open
-            my mind to creation and gives me a different perspective to look at
-            the world in a more beautiful way. About me, I am very enthusiastic
-            about learning new things and exploring nature. It is my passion to
-            learn about the world around me and to help people by working in the
-            field of programming.
-          </p>
-        </div>
+          <img src={image} className={style.aboutMe__profile} />
+          <div className={style.aboutMe__description}>
+            <h1 className={style.aboutMe__title}>About Me</h1>
+            <p>
+              I like to help.
+              Maybe I am in a wring direction
+            </p>
+          </div>
         </div>
 
         <div className={style.aboutMe__footer}>
